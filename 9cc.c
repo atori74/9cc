4edead1ffc024e8d78f6893d582b15622eb86b84
atori <x90.empty@outlook.com>
2025-03-21 03:13:39 +0900

    抽象構文木で書き直し

--- 9cc.c
+++ 9cc.c
@@ -7,23 +7,6 @@
 
 char *user_input;
 
-typedef enum {
-  TK_RESERVED,
-  TK_NUM,
-  TK_EOF,
-} TokenKind;
-
-typedef struct Token Token;
-
-struct Token {
-  TokenKind kind;
-  Token *next;
-  int val;
-  char *str;
-};
-
-Token *token;
-
 bool error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -45,28 +28,23 @@ void error_at(char *loc, char *fmt, ...) {
   exit(1);
 }
 
-bool consume(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    return false;
-  token = token->next;
-  return true;
-}
+/*
+ * トークンパーサー
+ */
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
 
-void expect(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    error_at(token->str, "'%c'ではありません", op);
-  token = token->next;
-}
+typedef struct Token Token;
 
-int expect_number() {
-  if (token->kind != TK_NUM)
-    error_at(token->str, "数ではありません");
-  int val = token->val;
-  token = token->next;
-  return val;
-}
-
-bool at_eof() { return token->kind == TK_EOF; }
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
 
 Token *new_token(TokenKind kind, Token *cur, char *str) {
   Token *tok = calloc(1, sizeof(Token));
@@ -105,6 +83,106 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
+Token *token;
+
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true; }
+
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error_at(token->str, "'%c'ではありません", op);
+  token = token->next;
+}
+
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error_at(token->str, "数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() { return token->kind == TK_EOF; }
+
+/*
+ * 再帰下降構文分析パーサー
+ */
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+
+// expr = num ("+" num | "-" num)*
+Node *expr() {
+	Node *node = new_node_num(expect_number());
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, new_node_num(expect_number()));
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, new_node_num(expect_number()));
+		else
+			return node;
+	}
+}
+
+/*
+ * スタックマシンコンパイラ
+ */
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
@@ -113,23 +191,15 @@ int main(int argc, char **argv) {
 
   user_input = argv[1];
   token = tokenize(user_input);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
